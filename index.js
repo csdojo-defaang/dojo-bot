edd406b1a4cd15d6e2873886ef1d0ecb74114c48
ykdojo <yksu4p@gmail.com>
2022-10-30 21:53:30 -0700

    created a setup script (index.js) for the bot

---
+++ index.js
@@ -0,0 +1,15 @@
+// Require the necessary discord.js classes
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
+
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// When the client is ready, run this code (only once)
+// We use 'c' for the event parameter to keep it separate from the already defined 'client'
+client.once(Events.ClientReady, c => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+// Log in to Discord with your client's token
+client.login(token);
